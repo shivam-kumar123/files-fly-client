5a7638aef5d7542f96c8518f1c802e9133beb771
shivam kumar <20051252@kiit.ac.in>
2023-11-10 15:40:36 +0530

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import QRCode from "react-qr-code";
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  const [fileName, SetFileName] = useState<string>('');
+  const [fileData, SetFileData] = useState<File | null>(null);
+  const [fileDownloadUrl, SetFileDownloadUrl] = useState<string>('');
+
+  const UploadFileToServer = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    const formData = new FormData();
+    formData.append('file', fileData as File); 
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_SERVER}/post`, formData);
+      SetFileDownloadUrl(res.data.downloadLink);
+      setTimeout(async () => {
+        await axios.delete(`${process.env.REACT_APP_SERVER}/delete/${res.data.fileId}`);
+      }, 300000);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  }
+  
+  const HandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files: File[] = Array.from(event.target.files || []);
+    SetFileName(files[0].name);
+    SetFileData(files[0]);
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Files Fly Too</h1>
+        <input
+          type = 'file'
+          onChange = {HandleChange}
+        />
+      <button onClick={UploadFileToServer}>Upload</button>
+      {fileName !== '' && <h3>{fileName}</h3>}
+      {
+        fileDownloadUrl !== '' && 
+        <div>
+          <h4>Download File From this URL or scan QR code (file can be downloaded within 10 minutes Only) :</h4>
+          <span>{fileDownloadUrl}</span>
+          <br /> <br />
+          <div>
+            <QRCode value={fileDownloadUrl} />
+          </div>
+        </div>
+      }
+
     </div>
   );
 }
